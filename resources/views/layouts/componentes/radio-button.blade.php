dd1d3ba423fa165fbfa1d23be5dda374e416c9ce
Leandro Fabri Pereira <leandro.fabri96@gmail.com>
2021-06-27 18:54:08 -0300

    limpa

--- resources/views/layouts/componentes/radio-button.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="custom-control custom-radio">
-    <input type="radio" value="{{$value ?? ""}}" {!! $custom_attribute ?? "" !!}  class="custom-control-input" id="{{$id ?? "defaultUnchecked"}}" name="{{$name ?? ""}}" {{!empty($checked) ? "checked":""}}>
-    <label class="custom-control-label" for="{{$id ?? "defaultUnchecked"}}">{{$text ?? ""}}</label>
-</div>
--- resources/views/layouts/componentes/table.blade.php
+++
@@ -1,8 +0,0 @@
-<table {{$function??""}} class="table table-striped table-bordered">
-    <thead>
-    {{$table_head ?? ""}}
-    </thead>
-    <tbody>
-    {{$table_body ?? ""}}
-    </tbody>
-</table>
